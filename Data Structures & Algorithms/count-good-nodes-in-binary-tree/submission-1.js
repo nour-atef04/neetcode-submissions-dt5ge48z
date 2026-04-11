a230f9da105fb823591166f88838de9886784ded
nour-atef04 <138708947+nour-atef04@users.noreply.github.com>
2026-04-11 19:28:36 +0200

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    goodNodes(root) {
+        let count = 0;
+        let q = [];
+
+        q.push([root, -Infinity]);
+
+        while(q.length !== 0){
+            let [node, max] = q.pop();
+            if(node.val >= max) count++;
+
+            if(node.left){
+                q.push([node.left, Math.max(max, node.val)]);
+            }
+            if(node.right){
+                q.push([node.right, Math.max(max, node.val)]);
+            }
+        }
+
+        return count;
+
+    }
+
+}
+
+
